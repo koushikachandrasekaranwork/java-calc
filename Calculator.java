3dd2c0ba38dcbd22a5024ad88fba84a67250f43b
koushika <koushikachandrasekaran.work@gmail.com>
2025-04-11 12:26:46 +0530

    Added mul() and div() methods to Calculator

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,17 @@ public class Calculator {
         return a - b;
     }
 
+    //method for multiplication
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    //method for division
+    public static int div(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Cannot divide by zero");
+        return a / b;
+    }
+
   
     public static void main(String[] args) {
         // Test the methods
@@ -18,7 +29,7 @@ public class Calculator {
 
         System.out.println("Addition: " + add(a, b));
         System.out.println("Subtraction: " + sub(a, b));
-       // System.out.println("Multiplication: " + mul(a, b));
-       // System.out.println("Division: " + div(a, b));
+        System.out.println("Multiplication: " + mul(a, b));
+        System.out.println("Division: " + div(a, b));
     }
 }
